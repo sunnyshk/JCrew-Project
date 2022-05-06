6a0299901c75e2afd0ee221c0b585f6d90f1b565
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2022-05-06 17:54:47 +0530

    men and women section created
    me and women sections with its various sub pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,35 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Women1 } from './components/Women/Files/Women-new-arrivals'
+import { ProductDetails } from './components/Product_Details/ProductDetails'
+import { WomenBestSellers } from './components/Women/Files/WomenBestSellers'
+import { WomenTopRated } from './components/Women/Files/WomenTopRated'
+import { WomenBrandWeLove } from './components/Women/Files/WomenBrandWeLove'
+import { WomenReImagined } from './components/Women/Files/WomenReImagined'
+import { WomenShopAll } from './components/Women/Files/WomenShopAll'
+import { MenNewArrival } from './components/Men/Files/MenNewArrival'
+import { MenTopRated } from './components/Men/Files/MenTopRated'
+import { MenBestSellers } from './components/Men/Files/BestSellersMen'
+import { MenBrandWeLove } from './components/Men/Files/MenBrandWeLove'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+     {/* <Women1/> */}
+     {/* <WomenBestSellers/> */}
+     {/* <WomenTopRated/> */}
+     {/* <WomenBrandWeLove/> */}
+     {/* <WomenReImagined/> */}
+     {/* <WomenShopAll/> */}
+    {/* <MenNewArrival/> */}
+    {/* <MenTopRated/> */}
+    {/* <MenBestSellers/> */}
+    <MenBrandWeLove/>
+    
     </div>
   )
 }
